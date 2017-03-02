e3e26a1578233d72ab9d32c6b321c7587f7da5f7
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-03-01 23:02:05 -0800

    提取所有单/双字节操作码451个元数据(部分信息)

--- src/cn/org/assembler/utils/操作码信息类.java
+++ src/cn/org/assembler/utils/操作码信息类.java
@@ -1,8 +1,14 @@
 package cn.org.assembler.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.org.assembler.指令类;
+
 public class 操作码信息类 {
-  // TODO: 改为int
-  String 值;
+  int 值;
   
-  // TODO: 添加操作码对应的指令(可能>1)
+  // TODO: 应该为指令指令信息/元数据类
+  List<指令类> 指令 = new ArrayList<>();
+  int 操作码字节数;
 }
--- test/操作码元数据处理测试类.java
+++ test/操作码元数据处理测试类.java
@@ -8,7 +8,7 @@ public class 操作码元数据处理测试类 {
 
   @Test
   public void test() {
-    assertTrue("指令信息不应为空", 操作码元数据处理类.提取指令信息().size()>0);
+    assertEquals("指令信息不应为空", 451, 操作码元数据处理类.提取操作码信息().size());
   }
 
 }
