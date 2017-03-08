afef5e475373021f52bf495b797a75bd185a5d83
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-03-07 23:51:53 -0800

    确认MOV元数据(B8)成功从XML提取

--- test/操作码元数据处理测试类.java
+++ test/操作码元数据处理测试类.java
@@ -1,4 +1,3 @@
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -18,8 +17,8 @@ public class 操作码元数据处理测试类 {
   public void 单操作码() {
     List<操作码元数据类> 操作码元数据 = 操作码元数据处理类.提取操作码信息("资源/测试单操作码元数据.xml");
     assertEquals("指令信息不应为空", 2, 操作码元数据.size());
-    
-    for(操作码元数据类 某操作码元数据 : 操作码元数据) {
+
+    for (操作码元数据类 某操作码元数据 : 操作码元数据) {
       assertTrue(某操作码元数据.值 >= 0);
       assertTrue(某操作码元数据.值 <= 255);
 
@@ -28,10 +27,11 @@ public class 操作码元数据处理测试类 {
   }
 
   @Test
-  public void test() {
+  public void 全部操作码() {
     List<操作码元数据类> 操作码元数据 = 操作码元数据处理类.提取操作码信息();
     assertEquals("指令信息不应为空", 451, 操作码元数据.size());
 
+    boolean 存在操作符 = false;
     for (操作码元数据类 元数据 : 操作码元数据) {
       assertTrue("操作码值过小:" + 元数据.值, 元数据.值 >= 0);
       assertTrue("操作码值过大:" + 元数据.值, 元数据.值 <= 255);
@@ -41,13 +41,19 @@ public class 操作码元数据处理测试类 {
         System.out.println("无指令的操作码: " + Integer.toHexString(元数据.值) + " " + 元数据.操作码字节数 + "字节");
       } else {
         for (指令元数据类 某指令元数据 : 指令元数据) {
-            for (指令格式类 某格式 : 某指令元数据.格式) {
-              assertNotNull(某格式.助记符);
+          for (指令格式类 某格式 : 某指令元数据.格式) {
+            assertNotNull(某格式.助记符);
+
+            // TODO: 将所有待验证指令置于对应表中
+            if (Integer.toHexString(元数据.值).equalsIgnoreCase("B8") && 某格式.助记符.equals("MOV")) {
+              存在操作符 = true;
             }
           }
-        
+        }
+
       }
     }
+    assertTrue(存在操作符);
   }
 
 }
