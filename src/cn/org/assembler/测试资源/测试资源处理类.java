1420002b6c992cc03f5a0a79eb7d543e2c0ad456
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-03-19 17:05:23 -0700

    遍历所有测试源码,已识别200行代码

--- src/cn/org/assembler/测试资源/测试资源处理类.java
+++ src/cn/org/assembler/测试资源/测试资源处理类.java
@@ -4,9 +4,16 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import cn.org.assembler.utils.文件功能类;
+
 public class 测试资源处理类 {
 
   private static final String 测试文件路径 = "测试/指令/";
+  private static final String 汇编文件扩展名 = ".asm";
+
+  public static List<String> 取代码行(String 源码文件名) {
+    return 文件功能类.读取行(测试文件路径 + 源码文件名 + 汇编文件扩展名);
+  }
 
   public static List<String> 取所有测试源码文件名() {
     List<String> 文件名列表 = new ArrayList<>();
@@ -14,7 +21,7 @@ public class 测试资源处理类 {
     if (测试资源目录.isDirectory()) {
       for (File 文件 : 测试资源目录.listFiles()) {
         String 文件名 = 文件.getName();
-        if (文件.isFile() && 文件名.endsWith(".asm")) {
+        if (文件.isFile() && 文件名.endsWith(汇编文件扩展名)) {
           文件名列表.add(文件名.substring(0, 文件名.length() - 4));
         }
       }
--- test/cn/org/assembler/模型/代码行测试类.java
+++ test/cn/org/assembler/模型/代码行测试类.java
@@ -1,25 +1,32 @@
 package cn.org.assembler.模型;
+
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
 import org.junit.Test;
 
-import cn.org.assembler.utils.文件功能类;
-import cn.org.assembler.模型.代码行类;
+import cn.org.assembler.测试资源.测试资源处理类;
 
 public class 代码行测试类 {
 
-  private static final String 测试文件路径 = "测试/指令/";
   @Test
   public void test() {
-    List<String> 行 = 文件功能类.读取行(测试文件路径 + "imm64.asm");
-    assertTrue("测试数据不应为空", 行.size() > 0);
-    
-    for(String 单行 : 行) {
-      代码行类 代码行 = 代码行类.分析(单行);
-      System.out.println(代码行);
+    int 已识别代码行数 = 0;
+    List<String> 源码文件名列表 = 测试资源处理类.取所有测试源码文件名();
+    for (String 源码文件名 : 源码文件名列表) {
+      List<String> 行 = 测试资源处理类.取代码行(源码文件名);
+      assertTrue("测试数据不应为空", 行.size() > 0);
+
+      for (String 单行 : 行) {
+        代码行类 代码行 = 代码行类.分析(单行);
+        if (代码行 != null) {
+          已识别代码行数++;
+        }
+      }
     }
+    assertTrue(已识别代码行数 > 0);
+    System.out.println("已识别代码行数: " + 已识别代码行数);
   }
 
 }
