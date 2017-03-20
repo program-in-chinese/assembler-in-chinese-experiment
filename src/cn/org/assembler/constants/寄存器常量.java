a68a6ec34651576d54b4e4f7d662073b12c42925
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-03-19 23:14:22 -0700

    添加所有四字/双字寄存器, 正确编译:10

--- src/cn/org/assembler/constants/寄存器常量.java
+++ src/cn/org/assembler/constants/寄存器常量.java
@@ -1,19 +1,57 @@
 package cn.org.assembler.constants;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class 寄存器常量 {
-  public static final String 四字节累加寄存器名 = "RAX";
+  public static final String 四字累加寄存器名 = "RAX";
+  public static final String 四字计数寄存器名 = "RCX";
+  public static final String 四字数据寄存器名 = "RDX";
+  public static final String 四字BX寄存器名 = "RBX";
+  public static final String 四字SP寄存器名 = "RSP";
+  public static final String 四字BP寄存器名 = "RBP";
+  public static final String 四字SI寄存器名 = "RSI";
+  public static final String 四字目标地址寄存器名 = "RDI";
+  
+  public static final String 双字节累加寄存器名 = "EAX";
+  public static final String 双字节计数寄存器名 = "ECX";
+  public static final String 双字节数据寄存器名 = "EDX";
+  public static final String 双字节BX寄存器名 = "EBX";
+  public static final String 双字节SP寄存器名 = "ESP";
+  public static final String 双字节BP寄存器名 = "EBP";
+  public static final String 双字节SI寄存器名 = "ESI";
+  public static final String 双字节目标地址寄存器名 = "EDI";
+  
   public static final HashSet<String> 四字节寄存器 = new HashSet<>();
+  public static final HashSet<String> 双字节寄存器 = new HashSet<>();
   
   private static final HashMap<String, Integer> 寄存器对应码 = new HashMap<>();
   
-  // Table 3-1, Intel® 64 and IA-32 architectures software developer’s manual combined volumes
+  // Table 3-1, Intel® 64 and IA-32 architectures software developer’s manual combined volumes Vol 2
   static {
-    寄存器对应码.put(四字节累加寄存器名, 0);
+    寄存器对应码.put(四字累加寄存器名, 0);
+    寄存器对应码.put(四字计数寄存器名, 1);
+    寄存器对应码.put(四字数据寄存器名, 2);
+    寄存器对应码.put(四字BX寄存器名, 3);
+    寄存器对应码.put(四字SP寄存器名, 4);
+    寄存器对应码.put(四字BP寄存器名, 5);
+    寄存器对应码.put(四字SI寄存器名, 6);
+    寄存器对应码.put(四字目标地址寄存器名, 7);
     
-    四字节寄存器.add(四字节累加寄存器名);
+    寄存器对应码.put(双字节累加寄存器名, 0);
+    寄存器对应码.put(双字节计数寄存器名, 1);
+    寄存器对应码.put(双字节数据寄存器名, 2);
+    寄存器对应码.put(双字节BX寄存器名, 3);
+    寄存器对应码.put(双字节SP寄存器名, 4);
+    寄存器对应码.put(双字节BP寄存器名, 5);
+    寄存器对应码.put(双字节SI寄存器名, 6);
+    寄存器对应码.put(双字节目标地址寄存器名, 7);
+    
+    四字节寄存器.addAll(Arrays.asList(四字累加寄存器名, 四字计数寄存器名, 四字数据寄存器名, 四字BX寄存器名, 四字SP寄存器名, 四字BP寄存器名,
+        四字SI寄存器名, 四字目标地址寄存器名));
+    双字节寄存器.addAll(Arrays.asList(双字节累加寄存器名, 双字节计数寄存器名, 双字节数据寄存器名, 双字节BX寄存器名, 双字节SP寄存器名, 双字节BP寄存器名,
+        双字节SI寄存器名, 双字节目标地址寄存器名));
   }
 
   /**
