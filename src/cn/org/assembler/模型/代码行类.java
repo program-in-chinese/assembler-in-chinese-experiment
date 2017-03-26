40ad5c04fc87bdd7d121b09b59aae6308a1bde45
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-03-25 22:50:29 -0700

    添加语法支持"strict 类型 立即数"

--- src/cn/org/assembler/模型/代码行类.java
+++ src/cn/org/assembler/模型/代码行类.java
@@ -21,7 +21,7 @@ public class 代码行类 {
     行 = 删除注释(行).trim();
     String 操作符格式 = "[A-Za-z]+";
     String 操作数1格式 = "[A-Za-z]+";
-    String 操作数2格式 = "[A-Za-z0-9]+";
+    String 操作数2格式 = "[A-Za-z0-9\\s]+";
     Pattern 格式 =
         Pattern.compile("^(" + 操作符格式 + ")\\s+(" + 操作数1格式 + "),\\s*(" + 操作数2格式 + ")$");
     Matcher 匹配器 = 格式.matcher(行);
@@ -30,6 +30,15 @@ public class 代码行类 {
       代码行.助记符 = 匹配器.group(1);
       代码行.操作数1 = 匹配器.group(2);
       代码行.操作数2 = 匹配器.group(3);
+
+      // 语法检查
+      // 比如操作数2: strict dword 35
+      if (代码行.操作数2.startsWith("strict")) {
+        if (代码行.操作数2.split(" ").length != 3) {
+          // 暂不支持省略类型的strict,比如strict 4
+          return null;
+        }
+      }
       return 代码行;
     }
     return null;
--- src/cn/org/assembler/模型/指令类.java
+++ src/cn/org/assembler/模型/指令类.java
@@ -45,8 +45,15 @@ public class 指令类 {
    */
   public static List<String> 生成二进制码(String 立即数字符, int 立即数位数) {
     List<String> 二进制码 = new ArrayList<>();
-    String 立即数 =
-        立即数字符.startsWith("0x") ? 立即数字符.substring(2) : Long.toHexString(Long.parseLong(立即数字符));
+    String 立即数 = 立即数字符;
+    if (立即数字符.startsWith("strict")) {
+      立即数 = 立即数.split(" ")[2];
+    }
+    if (立即数.startsWith("0x")) {
+      立即数 = 立即数.substring(2);
+    } else {
+      立即数 = Long.toHexString(Long.parseLong(立即数));
+    }
     if (立即数.length() % 2 == 1) {
       立即数 = "0" + 立即数;
     }
