2acb4df2d9ccdddc490dd50b60b5a1ff03302bf8
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-06-07 22:14:31 -0700

    windows subsystem暂时置为字符界面

--- src/cn/org/assembler/生成器类.java
+++ src/cn/org/assembler/生成器类.java
@@ -16,6 +16,7 @@ import org.boris.pecoff4j.SectionTable;
 import org.boris.pecoff4j.constant.Characteristics;
 import org.boris.pecoff4j.constant.MachineType;
 import org.boris.pecoff4j.constant.SectionFlag;
+import org.boris.pecoff4j.constant.WindowsSubsystem;
 import org.boris.pecoff4j.io.PEAssembler;
 import org.boris.pecoff4j.io.PEParser;
 
@@ -178,7 +179,24 @@ public class 生成器类 {
     必需头.setSizeOfImage(8192);
     必需头.setSizeOfHeaders(512);
     必需头.setCheckSum(15189);
-    必需头.setSubsystem(3);
+    
+    /* Windows Subsystem
+      IMAGE_SUBSYSTEM_UNKNOWN    0 An unknown subsystem
+      IMAGE_SUBSYSTEM_NATIVE    1 Device drivers and native Windows processes
+      IMAGE_SUBSYSTEM_WINDOWS_GUI   2 The Windows graphical user interface (GUI) subsystem
+      IMAGE_SUBSYSTEM_WINDOWS_CUI   3 The Windows character subsystem
+      IMAGE_SUBSYSTEM_OS2_CUI   5 The OS/2 character subsystem
+      IMAGE_SUBSYSTEM_POSIX_CUI     7 The Posix character subsystem
+      IMAGE_SUBSYSTEM_NATIVE_WINDOWS    8 Native Win9x driver
+      IMAGE_SUBSYSTEM_WINDOWS_CE_GUI    9 Windows CE
+      IMAGE_SUBSYSTEM_EFI_APPLICATION 10  An Extensible Firmware Interface (EFI) application
+      IMAGE_SUBSYSTEM_EFI_BOOT_ SERVICE_DRIVER    11  An EFI driver with boot services
+      IMAGE_SUBSYSTEM_EFI_RUNTIME_ DRIVER 12  An EFI driver with run-time services
+      IMAGE_SUBSYSTEM_EFI_ROM 13  An EFI ROM image
+      IMAGE_SUBSYSTEM_XBOX    14  XBOX
+      IMAGE_SUBSYSTEM_WINDOWS_BOOT_APPLICATION    16  Windows boot application.
+     */
+    必需头.setSubsystem(WindowsSubsystem.IMAGE_SUBSYSTEM_WINDOWS_CUI);
     必需头.setDllCharacteristics(0);
     必需头.setSizeOfStackReserve(4096);
     必需头.setSizeOfStackCommit(4096);
